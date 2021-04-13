50c2662901421c7861b616d052c6c37ab77c87cc
sksat <sksat@sksat.net>
2021-04-14 06:18:49 +0900

    serial

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@
 #![no_main]
 #![no_std]
 
+use core::fmt::Write;
+
 // Halt on panic
 use panic_halt as _; // panic handler
 
@@ -9,7 +11,7 @@ use cortex_m;
 use cortex_m_rt::entry;
 use stm32f4xx_hal as hal;
 
-use crate::hal::{prelude::*, stm32};
+use crate::hal::{prelude::*, serial, stm32};
 
 #[entry]
 fn main() -> ! {
@@ -28,7 +30,24 @@ fn main() -> ! {
         // Create a delay abstraction based on SysTick
         let mut delay = hal::delay::Delay::new(cp.SYST, clocks);
 
+        let tx_pin = gpioa.pa2.into_alternate_af7();
+        let rx_pin = gpioa.pa3.into_alternate_af7();
+
+        let serial = serial::Serial::usart2(
+            dp.USART2,
+            (tx_pin, rx_pin),
+            serial::config::Config::default().baudrate(9600.bps()),
+            clocks,
+        )
+        .unwrap();
+
+        let (mut tx, mut _rx) = serial.split();
+
+        let mut value: u8 = 0;
         loop {
+            writeln!(tx, "hello {}\r", value).unwrap();
+            value += 1;
+
             led.set_high().unwrap();
             delay.delay_ms(100_u32);
             led.set_low().unwrap();
